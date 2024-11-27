c2989f9e2c78d27220822af269c670bd16a812a6
kjbased <kjbass@outlook.com>
2024-11-26 19:51:18 -0700

    Upload to github

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pygame.Surface.fill(screen, (0, 0, 0))
+            pygame.Surface.fill(screen, (0, 0, 1))
             pygame.display.flip()
 
 if __name__ == "__main__":
