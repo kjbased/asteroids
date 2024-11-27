7d39d7b9b1dad2f1bf5bef09f50d36840794de88
kjbased <kjbass@outlook.com>
2024-11-26 22:10:08 -0700

    added new files

--- main.py
+++ main.py
@@ -3,19 +3,33 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print ("Starting asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    player= Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pygame.Surface.fill(screen, (0, 0, 1))
+            for item in updatable:
+                item.update(dt)
+            screen.fill("black")
+            for item in drawable:
+                item.draw(screen)
             pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
